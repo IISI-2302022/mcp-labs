5a784ac09cc9d37185fb1f1344dbf6ce37803239
mc.chen <mc.chen@iisigroup.com>
2026-04-26 10:08:38 +0800

    update

--- labs/03-spring-ai-mcp-auth-keycloak/spring-ai-mcp-client-security/src/main/java/com/ming/mymcpclient/controller/DemoController.java
+++ labs/03-spring-ai-mcp-auth-keycloak/spring-ai-mcp-client-security/src/main/java/com/ming/mymcpclient/controller/DemoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -33,13 +34,12 @@ class DemoController {
     }
 
     @GetMapping("/")
-    String index(@RequestParam(required = false) String query) {
+    String index(@RequestParam(required = false) String query, @RequestParam(required = false, defaultValue = "test") String conversationId) {
         var currentWeatherBlock = "";
         if (StringUtils.hasText(query)) {
             val chatResponse = chatClient.prompt(query)
-                    .advisors((advisorSpec) -> {
-                        advisorSpec.param(ChatMemory.CONVERSATION_ID, "test1");
-                    })
+                    .advisors((advisorSpec) ->
+                            advisorSpec.param(ChatMemory.CONVERSATION_ID, conversationId))
                     .call()
                     .content();
 
@@ -66,7 +66,11 @@ class DemoController {
                 
                 <p>請輸入你想問 AI 的問題</p>
                 <form action="" method="GET">
-                    <input type="text" name="query" value="" placeholder="請 AI 進行轉帳操作" />
+                    <label for="conversationId">ConversationId:</label>
+                    <input type="text" id="conversationId" name="conversationId" value="%s" placeholder="對話記憶 sessionId" />
+                    <br/>
+                    <label for="query">Query:</label>
+                    <input type="text" id="query" name="query" value="%s" placeholder="請 AI 進行轉帳操作" />
                     <button type="submit">Ask the LLM</button>
                 </form>
                 
@@ -76,7 +80,7 @@ class DemoController {
                 <ul>
                 %s
                 </ul>
-                """.formatted(currentWeatherBlock, currentMcpServersBlock);
+                """.formatted(currentWeatherBlock, conversationId, Optional.ofNullable(query).orElse(""), currentMcpServersBlock);
     }
 
 }
