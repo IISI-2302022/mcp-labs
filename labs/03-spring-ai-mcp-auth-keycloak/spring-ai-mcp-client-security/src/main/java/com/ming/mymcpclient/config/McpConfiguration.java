e10eab5267cdc1ef07e6de340ecfa33f5e872fe2
mc.chen <mc.chen@iisigroup.com>
2026-04-28 18:37:57 +0800

    update

--- labs/03-spring-ai-mcp-auth-keycloak/spring-ai-mcp-client-security/src/main/java/com/ming/mymcpclient/config/McpConfiguration.java
+++ labs/03-spring-ai-mcp-auth-keycloak/spring-ai-mcp-client-security/src/main/java/com/ming/mymcpclient/config/McpConfiguration.java
@@ -4,6 +4,7 @@ package com.ming.mymcpclient.config;
 import org.springaicommunity.mcp.security.client.sync.AuthenticationMcpTransportContextProvider;
 import org.springaicommunity.mcp.security.client.sync.oauth2.webclient.McpOAuth2AuthorizationCodeExchangeFilterFunction;
 import org.springframework.ai.mcp.customizer.McpSyncClientCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
@@ -18,8 +19,9 @@ class McpConfiguration {
     }
 
     @Bean
-    WebClient.Builder mcpWebClientBuilder(OAuth2AuthorizedClientManager clientManager) {
-        return WebClient.builder()
+    WebClientCustomizer webClientCustomizer(OAuth2AuthorizedClientManager clientManager) {
+        return webClientBuilder -> webClientBuilder
                 .filter(new McpOAuth2AuthorizationCodeExchangeFilterFunction(clientManager, "auth-code"));
     }
+
 }
